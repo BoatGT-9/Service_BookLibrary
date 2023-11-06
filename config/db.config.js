6dd75d9fa17b5a9d450ef9898a604a7838739c63
sofekung0726 <sofekung@gmail.com>
2023-11-06 14:01:01 +0700

    do auth and signin

--- config/db.config.js
+++ config/db.config.js
@@ -3,6 +3,13 @@ module.exports = {
     USER:"root",
     PASSWORD:"",
     DB:"book",
+    dialect:"mysql",
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    }
 }
 
 // const Sequelize = require("sequelize");
