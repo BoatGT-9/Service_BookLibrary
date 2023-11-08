15072a7d7d2ae4f31bb770e43013ffd2a8ceffaa
BoatGT-9 <twz192545@gmail.com>
2023-11-08 12:10:32 +0700

    ...

--- config/db.config.js
+++ config/db.config.js
@@ -4,29 +4,3 @@ module.exports = {
     PASSWORD:"",
     DB:"book",
 }
-
-// const Sequelize = require("sequelize");
-
-// const sequelize = new Sequelize("book", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-// });
-
-// module.exports = sequelize;
-
-// module.exports = 
-// {
-//   development: {
-//     HOST: 'localhost',
-//     USER: "root",
-//     PASSWORD: "",
-//     DB: "book",
-//     dialect: "mysql",  // This can be "mysql", "sqlite", "postgres", etc.
-//     pool:{
-//       max:5,
-//       min:0,
-//       acquire:30000,
-//       idle:10000
-//     }
-//   },
-// }
--- model/db.js
+++ model/db.js
@@ -1,35 +1,3 @@
-// const  {Sequelize}  = require("sequelize");
-// const dbConfig = require("../config/db.config");
-
-// const sequelize = new Sequelize(
-//   dbConfig.DB,
-//   dbConfig.HOST,
-//   dbConfig.USER,
-//   dbConfig.PASSWORD,
-//   {
-//     host: dbConfig.HOST,
-//     dialect: "mysql",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   }
-// );
-
-// async function connection() {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("connection has been SuccessFully");
-//   } catch (error) {
-//     console.log("Unable to connection ");
-//   }
-// }
-
-// connection();
-// module.exports = sequelize;
-
 const { Sequelize } = require("sequelize");
 const dbConfig = require("../config/db.config.js");
 
